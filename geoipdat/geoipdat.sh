f170738a48611daf2dc0e18eb030db7f6690d74f
Brett Kuskie <fullaxx@gmail.com>
2023-03-26 05:03:47 -0600

    Update geoipdat.sh

--- geoipdat/geoipdat.sh
+++ geoipdat/geoipdat.sh
@@ -11,16 +11,23 @@ PKGREV=${PKGREV:-rev}
 TMP="/tmp/${NAME}-mod-$$"
 if [ -e ${TMP} ]; then echo "${TMP} exists, exiting..." >&2; exit 1; fi
 
+EXDIR=`mktemp -d`
 mkdir -p ${TMP}/usr/share/mmdb
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz -O ${TMP}/usr/share/mmdb/GeoLite2-City.mmdb.gz
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz -O ${TMP}/usr/share/mmdb/GeoLite2-Country.mmdb.gz
-gunzip ${TMP}/usr/share/mmdb/GeoLite2-City.mmdb.gz
-gunzip ${TMP}/usr/share/mmdb/GeoLite2-Country.mmdb.gz
 
-# wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip -O ${TMP}/usr/share/mmdb/GeoLite2-City-CSV.zip
-# wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip -O ${TMP}/usr/share/mmdb/GeoLite2-Country-CSV.zip
-# ( set -e; cd ${TMP}/usr/share/mmdb/; unzip GeoLite2-City-CSV.zip; rm GeoLite2-City-CSV.zip )
-# ( set -e; cd ${TMP}/usr/share/mmdb/; unzip GeoLite2-Country-CSV.zip; rm GeoLite2-Country-CSV.zip )
+for ZIP in *.zip; do
+  unzip ${ZIP} -d ${EXDIR}
+done
+
+for TAR in *.tar.gz; do
+  tar xf ${TAR} -C ${EXDIR}
+done
+
+LFILE=`ls -1 ${EXDIR}/*/LICENSE.txt | head -n1`
+CFILE=`ls -1 ${EXDIR}/*/COPYRIGHT.txt | head -n1`
+rm -- ${EXDIR}/*/*-Locations-{de,es,fr,ja,pt-BR,ru,zh-CN}.csv
+cp -av ${LFILE} ${CFILE} ${TMP}/usr/share/mmdb/
+cp -av ${EXDIR}/*/*.{csv,mmdb} ${TMP}/usr/share/mmdb/
+rm -rf ${EXDIR}
 
 # Package up the modules and clean up
 dir2xzm ${TMP} ${PKG}-noarch-${PKGREV}.xzm
