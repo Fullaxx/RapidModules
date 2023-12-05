3a7095e43eefbd25eb8de82345e20b607fbe33ae
Brett Kuskie <fullaxx@gmail.com>
2023-12-05 06:10:48 -0700

    Add netdata-1.43.2.sh and portainer-2.19.3.sh

---
+++ dimgs/netdata-1.43.2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# https://github.com/netdata/netdata/tags
+# https://hub.docker.com/r/netdata/netdata/tags
+
+set -e
+
+NAME="netdata"
+VERS="1.43.2"
+PKG="${NAME}-${VERS}"
+TMP="/tmp/${NAME}-mod-$$"
+IDIR="${TMP}/usr/share/dimgs/${PKG}"
+
+IMAGE="netdata/netdata"
+TAG="v${VERS}"
+
+if [ `id -u` != "0" ]; then echo "Got Root?"; exit 1; fi
+
+# Pull Docker Image
+docker pull ${IMAGE}:${TAG}
+
+# Tag Docker Image
+docker tag ${IMAGE}:${TAG} ${IMAGE}:latest
+
+# Save Image, Script and Readme
+mkdir -p ${IDIR}
+docker save ${IMAGE}:latest -o ${IDIR}/${NAME}.tar
+
+wget https://raw.githubusercontent.com/netdata/netdata/master/README.md -O ${IDIR}/README.md
+
+cat << EOFF > ${IDIR}/run.sh
+#!/bin/bash
+
+docker load -i netdata.tar
+docker run -d \\
+--name=netdata \\
+-p 999:19999 \\
+-v netdataconfig:/etc/netdata \\
+-v netdatalib:/var/lib/netdata \\
+-v netdatacache:/var/cache/netdata \\
+-v /etc/passwd:/host/etc/passwd:ro \\
+-v /etc/group:/host/etc/group:ro \\
+-v /proc:/host/proc:ro \\
+-v /sys:/host/sys:ro \\
+-v /etc/os-release:/host/etc/os-release:ro \\
+--restart unless-stopped \\
+--cap-add SYS_PTRACE \\
+--security-opt apparmor=unconfined \\
+netdata/netdata:latest
+EOFF
+chmod 0700 ${IDIR}/run.sh
+
+# Package Module
+dir2xzm ${TMP} ${PKG}.xzm
+rm -rf ${PKG}
+rm -rf ${TMP}
+
+# EOF
---
+++ dimgs/portainer-2.19.3.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# https://github.com/netdata/netdata/tags
+# https://hub.docker.com/r/netdata/netdata/tags
+
+set -e
+
+NAME="portainer"
+VERS="2.19.3"
+PKG="${NAME}-${VERS}"
+TMP="/tmp/${NAME}-mod-$$"
+IDIR="${TMP}/usr/share/dimgs/${PKG}"
+
+IMAGE="portainer/portainer-ce"
+TAG="${VERS}"
+
+if [ `id -u` != "0" ]; then echo "Got Root?"; exit 1; fi
+
+# Pull Docker Image
+docker pull ${IMAGE}:${TAG}
+
+# Tag Docker Image
+docker tag ${IMAGE}:${TAG} ${IMAGE}:latest
+
+# Save Image, Script and Readme
+mkdir -p ${IDIR}
+docker save ${IMAGE}:latest -o ${IDIR}/${NAME}.tar
+
+# wget https://raw.githubusercontent.com/netdata/netdata/master/README.md -O ${IDIR}/README.md
+
+cat << EOFF > ${IDIR}/run.sh
+#!/bin/bash
+
+docker load -i portainer.tar
+docker run -d \\
+--ulimit 1048576:1048576 \\
+--name=portainer \\
+--restart=always \\
+-p 8000:8000 \\
+-p 9443:9443 \\
+-v /var/run/docker.sock:/var/run/docker.sock \\
+-v portainer_data:/data \\
+portainer/portainer-ce:latest
+EOFF
+chmod 0700 ${IDIR}/run.sh
+
+# Package Module
+dir2xzm ${TMP} ${PKG}.xzm
+rm -rf ${PKG}
+rm -rf ${TMP}
+
+# EOF
